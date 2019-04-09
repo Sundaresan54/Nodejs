26c737967cacf9b7016f58552419438c29ad29c3
Sundaresan Venugopal <sundargopal26@gmail.com>
2019-04-09 19:37:29 +0530

    changed Ui

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -19,7 +19,7 @@ this.setState({
   })
 }
 submitClick=(event) =>{
-if((this.state.username=="Sundar") &&   (this.state.password=="Sundar@123"))
+if((this.state.username=="sundar@gmail.com") &&   (this.state.password=="Sundar@123"))
 {
   console.log("logged in")
    this.setState({isLogined:true,
@@ -39,12 +39,25 @@ return (
 
 <div>
 <h1>BookStore App</h1>
-<input type="text" name="username" hint="username" value={this.state.username} onChange=    {this.handleInputChange} />
-<br/>
-<input type="password" name="password" hint="password" value = {this.state.passwordeseteset} onChange={this.handleInputChange} />
-<br/>
-<button style= {{marginRight:"20px"}}  name="submit" onClick={this.submitClick}> Submit</button>
-<button  name="Reset" onClick={this.handlePage}> Reset</button>
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={this.state.username} onChange=    {this.handleInputChange}/>
+    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"  value = {this.state.passwordeseteset} onChange={this.handleInputChange}/>
+  </div>
+  <div className="form-check">
+    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <div className="btn-group" role="group" aria-label="Basic example">
+  <button type="submit" className="btn btn-primary"  onClick={this.submitClick}>Submit</button>
+  <button  name="Reset"className="btn btn-primary" onClick={this.handlePage}> Reset</button>
+  </div>
+</form>
 </div>
 )
 }
