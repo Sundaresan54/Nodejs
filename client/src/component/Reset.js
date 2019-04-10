c7f455e654efdf0531a43304f5920b632b587c90
Sundaresan Venugopal <sundargopal26@gmail.com>
2019-04-10 08:41:25 +0530

    added snapshot

---
+++ client/src/component/Reset.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class Reset extends React.Component {
+
+render() {
+  return(
+    <div>
+    <input type="text" name="Enter email" value = " "  />
+    If you registered already please enter your Email.
+    </div>
+  )
+}
+}
